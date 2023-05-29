7df2706fd34e36edfc100e1f35ec6c4076cc22f3
YuriPetukhov <petuchowjuri@yahoo.de>
2023-05-29 15:14:39 +0300

    оптимизировал код

--- src/JavaCore/OOPPart2InterfacesAndPolymorphism/ServiceStation.java
+++ src/JavaCore/OOPPart2InterfacesAndPolymorphism/ServiceStation.java
@@ -3,17 +3,12 @@ package JavaCore.OOPPart2InterfacesAndPolymorphism;
 public class ServiceStation {
     public void check(MeansOfTransport meansOfTransport) {
         if (meansOfTransport != null) {
-            print(meansOfTransport);
+            System.out.println("Обслуживаем " + meansOfTransport.getModelName());
             for (int i = 0; i < meansOfTransport.getWheelsCount(); i++) {
                 meansOfTransport.updateTyre();
             }
             meansOfTransport.checkEngine();
             meansOfTransport.checkTrailer();
-
         }
-    }
-
-    public void print(MeansOfTransport meansOfTransport) {
-        System.out.println("Обслуживаем " + meansOfTransport.getModelName());
     }
 }
