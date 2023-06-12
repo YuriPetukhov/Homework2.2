4da5b64709ffa1d87777aa9ec7e591ef009b9acc
YuriPetukhov <petuchowjuri@yahoo.de>
2023-06-12 22:14:45 +0300

    Вставил PATTERN

--- src/JavaCore/Exception/UserRegistration.java
+++ src/JavaCore/Exception/UserRegistration.java
@@ -1,18 +1,21 @@
 package JavaCore.Exception;
+
 import JavaCore.Exception.Exceptions.*;
 
 public class UserRegistration {
     private static final int MAX_LOGIN_LENGTH = 20;
     private static final int MAX_PASSWORD_LENGTH = 20;
+    public static final String PATTERN = "[a-zA-Z0-9_]+";
 
-    public static void checkUserInfo(String login, String password, String confirmPassword) throws UserRegistrationException {
+    public static void checkUserInfo(String login, String password, String confirmPassword)
+            throws UserRegistrationException {
         // Проверка логина
-        if (login.length() > MAX_LOGIN_LENGTH || !login.matches("[a-zA-Z0-9_]+")) {
+        if (login.length() > MAX_LOGIN_LENGTH || !login.matches(PATTERN)) {
             throw new WrongLoginException("Wrong login");
         }
 
         // Проверка пароля
-        if (password.length() > MAX_PASSWORD_LENGTH || !password.matches("[a-zA-Z0-9_]+")) {
+        if (password.length() > MAX_PASSWORD_LENGTH || !password.matches(PATTERN)) {
             throw new WrongPasswordException("Wrong password");
         }
 
